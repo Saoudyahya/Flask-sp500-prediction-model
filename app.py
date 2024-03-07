93141ece06c92f58480faabd282b334061969bac
Nadif-Azeddine <nadifazzedine@gmail.com>
2024-03-07 15:23:09 +0100

    front

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request
 import model  # Import your script
-app = Flask(__name__)
+
+app = Flask(__name__, static_url_path='/static', static_folder='static')
+
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET'])
 def predict():
-    precision = model.main()  # Call your main function
-    return jsonify({'precision': precision})
+    if request.method == 'GET':
+        precision = model.main()  # Call your main function
+        return jsonify({'precision': precision})
 
 if __name__ == '__main__':
     app.run(debug=True)
